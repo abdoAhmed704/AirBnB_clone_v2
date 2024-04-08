7f0ac5d9d403459827eae842a8683af4c4a94974
Mo7ammad-safwat <MohammedGhaffar20648@azhar.edu.eg>
2024-04-07 20:40:46 -0700

    add

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,33 +1,38 @@
 #!/usr/bin/env bash
-# Sets up web servers for the deployment of web_static
+# Sets up a web server for deployment of web_static.
 
-# Install Nginx if it is not already installed
-sudo apt-get update
-sudo apt-get -y install nginx
+apt-get update
+apt-get install -y nginx
 
-# Create the required directories
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create a fake HTML file
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create a symbolic link, remove if it exists
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Give ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -R ubuntu:ubuntu /data/
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-# Update the Nginx configuration to serve the content
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
-# Restart Nginx to apply the changes
-sudo service nginx restart
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-exit 0
+service nginx restart
