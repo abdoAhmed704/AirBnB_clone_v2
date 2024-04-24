7fb9b59a3c3d3acb8ecac05be177f941f134ea6f
root <root@DESKTOP-HL0HK4V>
2024-04-24 16:56:05 +0200

    add web statc

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,31 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Script that sets up your web servers for the deployment of web_static.
 
-apt-get update
-apt-get install -y nginx
+SITE_FILE=/etc/nginx/sites-available/default
+END_POINT='\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n'
+HTML_CONTENT=\
+"<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+CHECK_HBNB=$(grep 'location /hbnb_static' < "$SITE_FILE")
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p '/data/web_static/releases/test/'
+sudo mkdir -p '/data/web_static/shared/'
+sudo touch '/data/web_static/releases/test/index.html'
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+echo "$HTML_CONTENT" > '/data/web_static/releases/test/index.html'
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+if [ -z "$CHECK_HBNB" ]; then
+	sudo sed -i "s@^\tserver_name _;@&$END_POINT@" $SITE_FILE
+fi
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+sudo service nginx restart
